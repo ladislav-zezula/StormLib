34dc0f6a1ca3a44e2eefb215296cf60e64747137
Ladislav <Zezula>
2013-03-16 19:05:54 +0100

    + Test

--- test/Test.cpp
+++ test/Test.cpp
@@ -2157,6 +2157,7 @@ int main(void)
     // Mix the random number generator
 //  srand(GetTickCount());
 
+    // Test for encrypted streams
 //  FileStream_OpenFile(_T("e:\\Hry\\StarCraft II\\Updates\\SC2_HotS_20_BGDL\\SC2_HotS_20_BGDL_deDE.MPQE"), STREAM_PROVIDER_ENCRYPTED | BASE_PROVIDER_FILE);
 
     // Test structure sizes
