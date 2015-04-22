0d19016cb8eddc21e0cceeae33361e8afb9e3192
Tim Kurvers <tim@moonsphere.net>
2015-04-22 16:13:58 +0200

    Define PDWORD in StormPort.h

--- src/StormPort.h
+++ src/StormPort.h
@@ -148,6 +148,7 @@
   typedef LONG         * PLONG;
   typedef DWORD        * LPDWORD;
   typedef BYTE         * LPBYTE;
+  typedef DWORD        * PDWORD;
 
   #ifdef PLATFORM_32BIT
     #define _LZMA_UINT32_IS_ULONG
